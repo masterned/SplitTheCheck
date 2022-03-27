da562b57a80485ee064b6b327c7ad5917469f5cb
Spencer Dent <sd00070@my.westga.edu>
2022-03-27 16:33:57 -0400

    set root route to restaurants index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'restaurants#index'
+
   resources :restaurants, only: [:index, :show, :new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
