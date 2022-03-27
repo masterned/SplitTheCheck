6e07b17b9c2744f8c131fc3a02722f2e6a968fcd
Spencer Dent <sd00070@my.westga.edu>
2022-03-27 05:35:11 -0400

    prevent delete route in controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[ show edit update ]
 
   # GET /restaurants or /restaurants.json
   def index
