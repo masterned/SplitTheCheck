afd3466cb547fd5c3290b80b480b6684b3d8c6c7
Spencer Dent <sd00070@my.westga.edu>
2022-03-27 02:53:18 -0400

    validate user input

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+    validates :name, :location, presence: true
+    validates :will_split_votes, :will_not_split_votes, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
 end
