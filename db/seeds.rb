90546823449994d62c1490ccb7c840fce8f32a06
Spencer Dent <sd00070@my.westga.edu>
2022-03-27 22:21:21 -0400

    re-seed db with demo values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Restaurant.delete_all
 
-# Restaurant.create!(name: 'Garden Grill', location: 'EPCOT: The Lands', will_split_votes: 6, will_not_split_votes: 1)
-# Restaurant.create!(name: 'Sunshine Seasons', location: 'EPCOT: The Lands')
-# Restaurant.create!(name: 'Space 220', location: 'EPCOT: Future World')
-# Restaurant.create!(name: 'Skipper Canteen', location: 'Magic Kingdom: Adventureland')
-# Restaurant.create!(name: 'Liberty Tree Tavern', location: 'Magic Kingdom: Liberty Square')
-# Restaurant.create!(name: 'The Brown Derby', location: 'Disney Studios: Hollywood Boulevard')
-# Restaurant.create!(name: 'Tiffins Restaurant', location: 'Animal Kingdom: Discovery Island')
+Restaurant.create!(name: 'Garden Grill', location: 'EPCOT: The Lands', will_split_votes: 6, will_not_split_votes: 1)
+Restaurant.create!(name: 'Sunshine Seasons', location: 'EPCOT: The Lands')
+Restaurant.create!(name: 'Space 220', location: 'EPCOT: Future World')
+Restaurant.create!(name: 'Skipper Canteen', location: 'Magic Kingdom: Adventureland')
+Restaurant.create!(name: 'Liberty Tree Tavern', location: 'Magic Kingdom: Liberty Square')
+Restaurant.create!(name: 'The Brown Derby', location: 'Disney Studios: Hollywood Boulevard')
+Restaurant.create!(name: 'Tiffins Restaurant', location: 'Animal Kingdom: Discovery Island')
+Restaurant.create!(name: 'Kona Cafe', location: 'Polynesian Village Resort')
+Restaurant.create!(name: 'Chef Mickey\'s', location: 'Contemporary Resort')
