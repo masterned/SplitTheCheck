17759d123f9ea0b8033c9ed73bcb1766c54bd334
Spencer Dent <sd00070@my.westga.edu>
2022-03-27 22:18:00 -0400

    handle empty Restaurants table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Restaurant.delete_all
 
-Restaurant.create!(name: 'Garden Grill', location: 'EPCOT: The Lands')
-Restaurant.create!(name: 'Sunshine Seasons', location: 'EPCOT: The Lands')
-Restaurant.create!(name: 'Space 220', location: 'EPCOT: Future World')
-Restaurant.create!(name: 'Skipper Canteen', location: 'Magic Kingdom: Adventureland')
-Restaurant.create!(name: 'Liberty Tree Tavern', location: 'Magic Kingdom: Liberty Square')
-Restaurant.create!(name: 'The Brown Derby', location: 'Disney Studios: Hollywood Boulevard')
-Restaurant.create!(name: 'Tiffins Restaurant', location: 'Animal Kingdom: Discovery Island')
+# Restaurant.create!(name: 'Garden Grill', location: 'EPCOT: The Lands', will_split_votes: 6, will_not_split_votes: 1)
+# Restaurant.create!(name: 'Sunshine Seasons', location: 'EPCOT: The Lands')
+# Restaurant.create!(name: 'Space 220', location: 'EPCOT: Future World')
+# Restaurant.create!(name: 'Skipper Canteen', location: 'Magic Kingdom: Adventureland')
+# Restaurant.create!(name: 'Liberty Tree Tavern', location: 'Magic Kingdom: Liberty Square')
+# Restaurant.create!(name: 'The Brown Derby', location: 'Disney Studios: Hollywood Boulevard')
+# Restaurant.create!(name: 'Tiffins Restaurant', location: 'Animal Kingdom: Discovery Island')
