c5af0bbcfab45dad7d32d17a9c8b8895728c1816
Spencer Dent <sd00070@my.westga.edu>
2022-04-17 23:11:39 -0400

    fix VotesController test

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @vote = votes(:one_gg_e)
+  end
+
   test "should create vote" do
-    skip
     
     assert_difference('Vote.count') do
-      post votes_url, params: { vote: { restaurant_id: @vote.restaurant_id, user_id: @vote.user_id, will_split?: @vote.will_split? } }
+      post votes_url, params: {  restaurant_id: @vote.restaurant_id, user_id: @vote.user_id, will_split?: @vote.will_split? }
     end
 
     assert_redirected_to restaurants_url
