40469095ab6983947c4c87268ad180c514433314
Spencer Dent <sd00070@my.westga.edu>
2022-04-17 22:59:35 -0400

    remove validation for non-existant fields

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
   validates :name, :location, presence: true
-  validates :will_split_votes, :will_not_split_votes, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :votes, dependent: :destroy
 
