615ab3bff78e0404af34b37dc459e30dd35088ef
Spencer Dent <sd00070@my.westga.edu>
2022-04-17 21:42:24 -0400

    remove references to non-existant methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update vote_will_split vote_will_not_split ]
+  before_action :set_restaurant, only: %i[ show edit update ]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /restaurants or /restaurants.json
