9aff51a4e7dac8648a69ec0052ffc7b52df9f457
Spencer Dent <sd00070@my.westga.edu>
2022-04-29 05:37:39 -0400

    implement comment to_s method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  def to_s
+    "on #{created_at.strftime("%Y-%m-%d")}, #{user.username} says: \"#{body}\""
+  end
 end
