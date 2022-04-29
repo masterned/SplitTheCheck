6edbe5cab715962455e43653f377b7287c2329b0
Spencer Dent <sd00070@my.westga.edu>
2022-04-29 05:04:49 -0400

    create comments table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_003958) do
+ActiveRecord::Schema.define(version: 2022_04_29_085816) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -43,6 +53,8 @@ ActiveRecord::Schema.define(version: 2022_04_18_003958) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "comments", "restaurants"
+  add_foreign_key "comments", "users"
   add_foreign_key "votes", "restaurants"
   add_foreign_key "votes", "users"
 end
