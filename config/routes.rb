85b7f2e41352c167f8d71b961f27f90d133b67b0
Spencer Dent <sd00070@my.westga.edu>
2022-04-29 22:07:07 -0400

    rename unfavorite route symbol

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :favorites, only: [:create]
 
   get '/search', to: 'restaurants#search', as: 'search'
-  delete '/favorites', to: 'favorites#destroy', as: 'destroy'
+  delete '/favorites', to: 'favorites#destroy', as: 'unfavorite'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
