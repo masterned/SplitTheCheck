d71e24b28ca7aea3f334a9277a662dc703faaceb
Spencer Dent <sd00070@my.westga.edu>
2022-05-02 10:49:21 -0400

    test comment's `to_s` method

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @one = comments(:one)
+    @two = comments(:two)
+  end
+
+  test "to_s returns comment summary" do
+    assert_equal "on #{Date.today}, Mr. One says: \"I am number one!\"", @one.to_s
+
+    assert_equal "on #{Date.today}, Number 2 says: \"Who do I work for?\"", @two.to_s
+  end
 end
