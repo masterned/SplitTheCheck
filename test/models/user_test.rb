6d550a51e0e70a76638608b01d100b7d07e2982c
Spencer Dent <sd00070@my.westga.edu>
2022-05-02 10:58:01 -0400

    test user's `favorited?` method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @gg_e = restaurants(:gg_e)
+    @gg_t = restaurants(:gg_t)
+  end
+
+  test 'returns whether the user has favorited a specific restaurant' do
+    assert @user.favorited? @gg_e
+    assert !(@user.favorited? @gg_t)
+  end
 end
