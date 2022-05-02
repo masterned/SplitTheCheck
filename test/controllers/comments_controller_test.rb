2dff8418a351c8489b0dc97b0972165fc27cade6
Spencer Dent <sd00070@my.westga.edu>
2022-05-02 13:30:48 -0400

    test comment action redirects

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -10,6 +10,9 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    get new_comment_url(restaurant_id: @gg_e)
+    assert_redirected_to new_user_session_url
+
     sign_in @user
 
     get new_comment_url(restaurant_id: @gg_e)
@@ -17,13 +20,22 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create comment" do
+    get new_comment_url(restaurant_id: @gg_e)
+    assert_redirected_to new_user_session_url
+
     sign_in @user
 
+    # The user must first select a restaurant to comment on
+    assert_no_difference('Comment.count') do
+      post comments_url, params: { comment: { body: @comment.body } }
+    end
+    assert_redirected_to restaurants_url
+
+    # The user is authenticated & has selected a restaurant
     assert_difference('Comment.count') do
       get new_comment_url(restaurant_id: @gg_e)
       post comments_url, params: { comment: { body: @comment.body } }
     end
-
     assert_redirected_to restaurant_url(Comment.last.restaurant_id)
   end
 end
